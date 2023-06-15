18a8ce2c61fba8abfe699fdf7b606bf4be8974ca
iron <63185020+Irr0n@users.noreply.github.com>
2023-06-15 21:12:32 +0000

    added double quote checker to csv

--- app_gui.py
+++ app_gui.py
@@ -46,7 +46,9 @@ def do_wait_info_dots(current_wait_info: str) -> str:
 
 def clean_query(query: str) -> str:
     if query[-1] == ',':
-        return query[0:-2]
+        return query[0:-1]
+    if "\"\"" in query:
+        return query.replace("\"\"",'')
 
 
 query_tab, database_tab = st.tabs(["Query", "Database"])
